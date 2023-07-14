dbf1bc4c49f36c667a747d28ffecef6c767162c3
lingtine <lingtine@gmail.com>
2023-07-14 09:51:49 +0700

    complete component footer

--- src/app/components/footer/footer.jsx
+++ src/app/components/footer/footer.jsx
@@ -1,7 +1,83 @@
 function Footer() {
   return (
-    <div className="container mx-auto">
-      <div className="">footer Page</div>
+    <div className="bg-black">
+      <div className="container mx-auto pt-20">
+        <div className="grid grid-cols-4 gap-21 mb-12">
+          <div className="">
+            <div className="my-2">
+              <h5 className="text-primary font-bold text-lg">Exclusive</h5>
+            </div>
+            <ul className="text-primary">
+              <li className="py-2">
+                <p>Subscribe</p>
+              </li>
+              <li className="py-2">
+                <p>Get 10% off your first order</p>
+              </li>
+            </ul>
+          </div>
+          <div className="">
+            <div className="my-2">
+              <h5 className="text-primary font-bold text-lg">Support</h5>
+            </div>
+            <ul className="text-primary">
+              <li className="py-2">
+                <p>111 Bijoy sarani, Dhaka, DH 1515, Bangladesh.</p>
+              </li>
+              <li className="py-2">
+                <p>exclusive@gmail.com</p>
+              </li>
+              <li className="py-2">
+                <p>+88015-88888-9999</p>
+              </li>
+            </ul>
+          </div>
+          <div className="">
+            <div className="my-2">
+              <h5 className="text-primary font-bold text-lg">Account</h5>
+            </div>
+            <ul className="text-primary">
+              <li cclassName="py-2">
+                <p>My Account</p>
+              </li>
+              <li className="py-2">
+                <p>Login / Register</p>
+              </li>
+              <li cclassName="py-2">
+                <p>Cart</p>
+              </li>
+
+              <li className="py-2">
+                <p>Shop</p>
+              </li>
+            </ul>
+          </div>
+          <div className="">
+            <div className="my-2">
+              <h5 className="text-primary font-bold text-lg">Quick Link</h5>
+            </div>
+            <ul className="text-primary">
+              <li className="py-2">
+                <p>Privacy Policy</p>
+              </li>
+              <li className="py-2">
+                <p>Terms Of Use</p>
+              </li>
+              <li className="py-2">
+                <p>FAQ</p>
+              </li>
+              <li className="py-2">
+                <p>Contact</p>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="py-4">
+          <p className="text-primary text-center">
+            @ Copyright Rimel 2022. All right reserved
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
