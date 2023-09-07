40dea4d3c5c7b444b9772a47bae22c235c837c31
lingtine <lingtine@gmail.com>
2023-09-07 19:38:45 +0700

    update production

--- src/app/(client)/(private)/wish-list/page.jsx
+++ src/app/(client)/(private)/wish-list/page.jsx
@@ -1,59 +1,51 @@
 "use client";
 import Link from "next/link";
-import BoxProducts from "~/app/components/home-page/box-products";
-import Button from "~/app/components/button/button";
-import { useGetWishListQuery } from "~/redux/services/customer/customer-api";
+
 import { useSelector } from "react-redux";
 import { BsFillBagHeartFill } from "react-icons/bs";
-import ProductList from "~/app/components/products/product-list";
+
 import { useEffect } from "react";
 
 function WishList() {
-  const user = useSelector((state) => state.user);
-  const { wishList } = useSelector((state) => state.wishListSlice);
-  let renderWishList;
+  // let renderWishList;
   useEffect(() => {
     document.title = "Wish List";
   }, []);
-  const handleRemove = () => {};
-  if (wishList) {
-    if (wishList.length !== 0)
-      renderWishList = (
-        <div className="">
-          <div className="flex justify-between items-center my-8">
-            <div>
-              <h3 className="text-xl">WishList({wishList.length})</h3>
-            </div>
-            <div></div>
-          </div>
-        </div>
-      );
-    else {
-      renderWishList = (
-        <div className="text-4xl flex-col flex justify-center items-center my-60">
-          <div className="flex">
-            <BsFillBagHeartFill />
-            <h4 className="mx-8">The wishList is empty</h4>
-          </div>
-          <div className="my-12">
-            <Link
-              className="text-xl border py-4 px-8 rounded-md bg-secondary-3 text-primary"
-              href={"/"}
-            >
-              Continue shopping
-            </Link>
-          </div>
-        </div>
-      );
-    }
-  }
+
+  // if (wishList) {
+  //   if (wishList.length !== 0)
+  //     renderWishList = (
+  //       <div className="">
+  //         <div className="flex justify-between items-center my-8">
+  //           <div>
+  //             <h3 className="text-xl">WishList({wishList.length})</h3>
+  //           </div>
+  //           <div></div>
+  //         </div>
+  //       </div>
+  //     );
+  //   else {
+  //     renderWishList = (
+
+  //     );
+  //   }
+  // }
   return (
     <div className="container mx-auto my-16">
-      {renderWishList}
-
-      {/* <BoxProducts type={"Just For You"}>
-        <ProductList products={data.products || []} />
-      </BoxProducts> */}
+      <div className="text-4xl flex-col flex justify-center items-center my-60">
+        <div className="flex">
+          <BsFillBagHeartFill />
+          <h4 className="mx-8">The wishList is empty</h4>
+        </div>
+        <div className="my-12">
+          <Link
+            className="text-xl border py-4 px-8 rounded-md bg-secondary-3 text-primary"
+            href={"/"}
+          >
+            Continue shopping
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
